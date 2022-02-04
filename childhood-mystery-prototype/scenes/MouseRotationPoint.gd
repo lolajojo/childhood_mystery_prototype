31e75eb38655d9c68f198ff0ccecbdf04b9c72ff
Jojo Lolando <lolajojo@tutanota.com>
2022-02-04 23:52:11 +0300

    Add simple limits for vertical camera control

--- childhood-mystery-prototype/scenes/MouseRotationPoint.gd
+++ childhood-mystery-prototype/scenes/MouseRotationPoint.gd
@@ -4,4 +4,5 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		var movement = event.relative
 		rotation.x += -deg2rad(movement.y)
+		rotation.x = clamp(rotation.x, deg2rad(-90), deg2rad(90))
 		rotation.y += -deg2rad(movement.x)
