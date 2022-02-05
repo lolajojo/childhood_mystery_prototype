7dadb15873d17a3cbb4b12d1e7b9261a1cb07fa1
Jojo Lolando <lolajojo@tutanota.com>
2022-02-05 20:27:49 +0300

    Connect player movement to camera

--- childhood-mystery-prototype/scenes/Player.gd
+++ childhood-mystery-prototype/scenes/Player.gd
@@ -17,23 +17,24 @@ func _enter_tree():
 
 
 func _physics_process(delta):
-	correct_rotation()
+	if is_moving:
+		correct_rotation()
 	
 	var direction = Vector3.ZERO
 	
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+	if Input.is_action_pressed("move_right"):
+		direction.x -= 1
 	
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_back"):
 		direction.z += 1
+	if Input.is_action_pressed("move_back"):
+		direction.z -= 1
 	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		# $Pivot.look_at(-(translation + direction), Vector3.UP)
+		direction = direction.rotated(Vector3.UP, get_node(pivot_point).rotation.y)
 		
 		if not is_moving:
 			is_moving = true
