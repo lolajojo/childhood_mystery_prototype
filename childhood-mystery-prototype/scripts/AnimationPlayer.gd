636e03889664fee9a9667eedb75aa37a6ea9c809
Jojo Lolando <lolajojo@tutanota.com>
2022-02-05 18:03:42 +0300

    Add quitting on pressing Esc button and reduced time for switching between animations

--- childhood-mystery-prototype/scripts/AnimationPlayer.gd
+++ childhood-mystery-prototype/scripts/AnimationPlayer.gd
@@ -3,9 +3,9 @@ extends AnimationPlayer
 
 func _on_Player_idling():
 	get_animation("idle").loop = true
-	play("idle")
+	play("idle", 0.1)
 
 
 func _on_Player_start_moving():
 	get_animation("walking").loop = true
-	play("walking")
+	play("walking", 0.01)
