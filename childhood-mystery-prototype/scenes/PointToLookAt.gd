654a52c8dc8994c08a8584b25a1aca4a27f9302c
Jojo Lolando <lolajojo@tutanota.com>
2022-02-07 23:31:30 +0300

    Add rotation lock for all axis

--- childhood-mystery-prototype/scenes/PointToLookAt.gd
+++ childhood-mystery-prototype/scenes/PointToLookAt.gd
@@ -5,6 +5,8 @@ export(NodePath) var skeleton_path
 export(String) var bone_name
 export(float) var min_angle = deg2rad(-90)
 export(float) var max_angle = deg2rad(90)
+export(Vector3) var max_rotation = Vector3(45, 90, 30)
+export(Vector3) var min_rotation = Vector3(-45, -90, -30)
 
 var skeleton_node: Skeleton
 
@@ -26,11 +28,34 @@ func _physics_process(delta):
 
 
 func correct_basis_with_limits(basis: Basis):
-	var rotationAngle = basis.get_euler().y
+	var x_rotation_angle = basis.get_euler().x
+	basis = correct_basis_within_limits(basis, 
+										basis.x, 
+										x_rotation_angle, 
+										deg2rad(min_rotation.x), 
+										deg2rad(max_rotation.x))
 	
-	if rotationAngle > max_angle:
-		basis = basis.rotated(basis.y, -(rotationAngle - max_angle))
-	elif rotationAngle < min_angle:
-		basis = basis.rotated(basis.y, min_angle - rotationAngle)
+	var y_rotation_angle = basis.get_euler().y
+	basis = correct_basis_within_limits(basis, 
+										basis.y, 
+										y_rotation_angle, 
+										deg2rad(min_rotation.y), 
+										deg2rad(max_rotation.y))
+	
+	var z_rotation_angle = basis.get_euler().z
+	basis = correct_basis_within_limits(basis, 
+										basis.y, 
+										z_rotation_angle, 
+										deg2rad(min_rotation.z), 
+										deg2rad(max_rotation.z))
+	
+	return basis
+
+
+func correct_basis_within_limits(basis: Basis, axis, rotation_angle, min_angle, max_angle):
+	if rotation_angle > max_angle:
+		basis = basis.rotated(axis, -(rotation_angle - max_angle))
+	elif rotation_angle < min_angle:
+		basis = basis.rotated(axis, min_angle - rotation_angle)
 	
 	return basis
