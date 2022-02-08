2fb217822170fca06589d39767064b6f141c10a2
Jojo Lolando <lolajojo@tutanota.com>
2022-02-08 21:19:23 +0300

    Extract rotation limiting logic to the utility class

---
+++ childhood-mystery-prototype/scripts/PointToLookAt.gd
@@ -0,0 +1,31 @@
+extends Position3D
+
+
+export(NodePath) var skeleton_path
+export(String) var bone_name
+export(Vector3) var min_rotation = Vector3(-45, -90, -30)
+export(Vector3) var max_rotation = Vector3(45, 90, 30)
+
+var skeleton_node: Skeleton
+
+
+func _ready():
+	skeleton_node = get_node(skeleton_path)
+
+
+func _physics_process(delta):
+	if skeleton_node == null:
+		return
+	
+	var bone_id = skeleton_node.find_bone(bone_name)
+	var target_position = skeleton_node.global_transform.xform_inv(global_transform.origin)
+	
+	var rest_global_pose: Transform = skeleton_node.get_bone_global_pose(bone_id)
+	rest_global_pose = rest_global_pose.looking_at(target_position, Vector3.UP)
+	rest_global_pose.basis = rest_global_pose.basis.rotated(rest_global_pose.basis.y, deg2rad(180.0))
+	rest_global_pose.basis = LimitBasisCorrector.new().correct_basis_with_limits(
+		rest_global_pose.basis,
+		min_rotation, 
+		max_rotation)
+	
+	skeleton_node.set_bone_global_pose_override(bone_id, rest_global_pose, 1.0, true)
---
+++ childhood-mystery-prototype/scripts/utils/LimitBasisCorrector.gd
@@ -0,0 +1,39 @@
+extends Object
+
+
+class_name LimitBasisCorrector
+
+
+func correct_basis_with_limits(basis: Basis, min_rotation: Vector3, max_rotation: Vector3):
+	var x_rotation_angle = basis.get_euler().x
+	basis = _correct_basis_within_limits(basis, 
+										basis.x, 
+										x_rotation_angle, 
+										deg2rad(min_rotation.x), 
+										deg2rad(max_rotation.x))
+	
+	var y_rotation_angle = basis.get_euler().y
+	basis = _correct_basis_within_limits(basis, 
+										basis.y, 
+										y_rotation_angle, 
+										deg2rad(min_rotation.y), 
+										deg2rad(max_rotation.y))
+	
+	var z_rotation_angle = basis.get_euler().z
+	basis = _correct_basis_within_limits(basis, 
+										basis.y, 
+										z_rotation_angle, 
+										deg2rad(min_rotation.z), 
+										deg2rad(max_rotation.z))
+	
+	return basis
+
+
+func _correct_basis_within_limits(basis: Basis, axis, rotation_angle, min_angle, max_angle):
+	if rotation_angle > max_angle:
+		basis = basis.rotated(axis, -(rotation_angle - max_angle))
+	elif rotation_angle < min_angle:
+		basis = basis.rotated(axis, min_angle - rotation_angle)
+	
+	return basis
+
