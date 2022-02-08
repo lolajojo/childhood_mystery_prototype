5ee4d47fb1f9607368f3e9884e096c72d160abda
Jojo Lolando <lolajojo@tutanota.com>
2022-02-08 21:36:00 +0300

    Add freeing Object on leaving tree

--- childhood-mystery-prototype/scripts/PointToLookAt.gd
+++ childhood-mystery-prototype/scripts/PointToLookAt.gd
@@ -7,11 +7,14 @@ export(Vector3) var min_rotation = Vector3(-45, -90, -30)
 export(Vector3) var max_rotation = Vector3(45, 90, 30)
 
 var skeleton_node: Skeleton
-
+var basis_corrector: LimitBasisCorrector
 
 func _ready():
 	skeleton_node = get_node(skeleton_path)
+	basis_corrector = LimitBasisCorrector.new()
 
+func _exit_tree():
+	basis_corrector.free()
 
 func _physics_process(delta):
 	if skeleton_node == null:
@@ -23,7 +26,7 @@ func _physics_process(delta):
 	var rest_global_pose: Transform = skeleton_node.get_bone_global_pose(bone_id)
 	rest_global_pose = rest_global_pose.looking_at(target_position, Vector3.UP)
 	rest_global_pose.basis = rest_global_pose.basis.rotated(rest_global_pose.basis.y, deg2rad(180.0))
-	rest_global_pose.basis = LimitBasisCorrector.new().correct_basis_with_limits(
+	rest_global_pose.basis = basis_corrector.correct_basis_with_limits(
 		rest_global_pose.basis,
 		min_rotation, 
 		max_rotation)
