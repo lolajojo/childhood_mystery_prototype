c21c1b764f09e849bb5f47708946c4cbcb17abc0
Jojo Lolando <lolajojo@tutanota.com>
2022-02-12 18:32:13 +0300

    Add test for Y rotation

--- childhood-mystery-prototype/scripts/utils/LimitBasisCorrector.gd
+++ childhood-mystery-prototype/scripts/utils/LimitBasisCorrector.gd
@@ -8,6 +8,8 @@ func correct_basis_with_limits(
 	basis: Basis,
 	min_rotation: Vector3, 
 	max_rotation: Vector3) -> Basis:
+	
+	print("x rotation")
 	var x_rotation_angle = basis.get_euler().x
 	basis = _correct_basis_within_limits(basis, 
 										basis.x, 
@@ -15,6 +17,7 @@ func correct_basis_with_limits(
 										deg2rad(min_rotation.x), 
 										deg2rad(max_rotation.x))
 	
+	print("y rotation")
 	var y_rotation_angle = basis.get_euler().y
 	basis = _correct_basis_within_limits(basis, 
 										basis.y, 
@@ -22,6 +25,7 @@ func correct_basis_with_limits(
 										deg2rad(min_rotation.y), 
 										deg2rad(max_rotation.y))
 	
+	print("z rotation")
 	var z_rotation_angle = basis.get_euler().z
 	basis = _correct_basis_within_limits(basis, 
 										basis.y, 
@@ -38,10 +42,15 @@ func _correct_basis_within_limits(
 	rotation_angle,
 	min_angle,
 	max_angle) -> Basis:
+	
+	print("Starting rotation for basis: ", basis, " with rotation angle: ", rad2deg(rotation_angle))
 	if rotation_angle > max_angle:
 		basis = basis.rotated(axis, -(rotation_angle - max_angle))
+		basis = Basis()
+		print("Fixing basis because exceeded: ", basis, "deg diff: ", -rad2deg(rotation_angle - max_angle))
 	elif rotation_angle < min_angle:
 		basis = basis.rotated(axis, min_angle - rotation_angle)
+		print("Fixing basis because not enough: ", basis, "deg diff: ", rad2deg(min_angle - rotation_angle))
 	
 	return basis
 
--- childhood-mystery-prototype/tests/basis_limiter_tests.gd
+++ childhood-mystery-prototype/tests/basis_limiter_tests.gd
@@ -2,7 +2,7 @@ extends WAT.Test
 
 var sut: LimitBasisCorrector
 
-const min_rotation = Vector3(-PI/2, -PI/2, -PI/2)
+const min_rotation = Vector3(-rad2deg(PI/2), -rad2deg(PI/2), -rad2deg(PI/2))
 const max_rotation = -min_rotation
 
 func pre():
@@ -14,12 +14,12 @@ func post():
 
 
 func test_rotation_exceeded_left():
-	var expected_basis = Basis(Vector3.UP, PI/2)
+	var expected_basis = Basis(Vector3.UP, -PI / 2)
 	var basis = Basis(Vector3.UP, PI)
 	
-	var result = sut.correct_basis_with_limits(
+	basis = sut.correct_basis_with_limits(
 		basis, 
 		min_rotation, 
 		max_rotation)
 	
-	asserts.is_equal(expected_basis, result)
+	asserts.is_true(expected_basis.is_equal_approx(basis))
