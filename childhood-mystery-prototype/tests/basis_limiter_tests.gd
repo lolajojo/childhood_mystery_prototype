c97369e0875981a9e0804921760b3ab98817a751
Jojo Lolando <lolajojo@tutanota.com>
2022-02-12 23:40:53 +0300

    Add tests for each known cases

--- childhood-mystery-prototype/tests/basis_limiter_tests.gd
+++ childhood-mystery-prototype/tests/basis_limiter_tests.gd
@@ -12,56 +12,97 @@ func pre():
 func post():
 	sut.free()
 
-
-func test_rotation_above_limit_x_normale():
+# Testing above max limit roation cases
+func test_rotation_above_max_limit_x_normale():
 	var expected_basis = Basis(Vector3.RIGHT, PI / 2)
 	var basis = Basis(Vector3.RIGHT, deg2rad(160))
 	
-	basis = sut.correct_basis_with_limits(
-		basis, 
-		min_rotation, 
-		max_rotation)
-	
-	asserts.is_true(
-		expected_basis.is_equal_approx(basis),
-		"%s is not equal to %s" % [str(expected_basis), str(basis)])
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
 
 
-func test_rotation_under_limit_x_normale():
-	var expected_basis = Basis(Vector3.RIGHT, - PI / 2)
-	var basis = Basis(Vector3.RIGHT, deg2rad(-160))
-	
-	basis = sut.correct_basis_with_limits(
-		basis, 
-		min_rotation, 
-		max_rotation)
-	
-	asserts.is_true(
-		expected_basis.is_equal_approx(basis),
-		"%s is not equal to %s" % [str(expected_basis), str(basis)])
-
-
-func test_rotation_exceeded_y_normale():
+func test_rotation_above_max_limit_y_normale():
 	var expected_basis = Basis(Vector3.UP, deg2rad(90))
 	var basis = Basis(Vector3.UP, deg2rad(160))
 	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+
+func test_rotation_above_max_limit_z_normale():
+	var expected_basis = Basis(Vector3.FORWARD, PI / 2)
+	var basis = Basis(Vector3.FORWARD, deg2rad(160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+# Testing above limit rotation with additional 360 degrees
+func test_rotation_above_limit_plus_2pi_x_normale():
+	var expected_basis = Basis(Vector3.RIGHT, PI * 2 + PI / 2)
+	var basis = Basis(Vector3.RIGHT, deg2rad(160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+
+func test_rotation_above_limit_plus_2pi_y_normale():
+	var expected_basis = Basis(Vector3.UP, PI * 2 + PI / 2)
+	var basis = Basis(Vector3.UP, deg2rad(160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+
+func test_rotation_above_limit_plus_2pi_z_normale():
+	var expected_basis = Basis(Vector3.FORWARD, PI * 2 + PI / 2)
+	var basis = Basis(Vector3.FORWARD, deg2rad(160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+# Testing under min limit rotation cases
+func test_rotation_under_min_limit_x_normale():
+	var expected_basis = Basis(Vector3.RIGHT, - PI / 2)
+	var basis = Basis(Vector3.RIGHT, deg2rad(-160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+
+func test_rotation_under_min_limit_y_normale():
+	var expected_basis = Basis(Vector3.UP, - PI / 2)
+	var basis = Basis(Vector3.UP, deg2rad(-160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+
+func test_rotation_under_min_limit_z_normale():
+	var expected_basis = Basis(Vector3.FORWARD, - PI / 2)
+	var basis = Basis(Vector3.FORWARD, deg2rad(-160))
+	
+	_run_correction_with_assertion(
+		expected_basis,
+		basis)
+
+# Tests Utility
+func _run_correction_with_assertion(
+		expected_basis: Basis,
+		basis: Basis):
+	
 	basis = sut.correct_basis_with_limits(
 		basis, 
 		min_rotation, 
 		max_rotation)
 	
-	asserts.is_true(expected_basis.is_equal_approx(basis),
-		"%s is not equal to %s" % [str(expected_basis), str(basis)])
-
-
-func test_rotation_exceeded_z_normale():
-	var expected_basis = Basis(Vector3.BACK, PI / 2)
-	var basis = Basis(Vector3.BACK, deg2rad(160))
-	
-	basis = sut.correct_basis_with_limits(
-		basis, 
-		min_rotation, 
-		max_rotation)
-	
-	asserts.is_true(expected_basis.is_equal_approx(basis),
+	asserts.is_true(
+		expected_basis.is_equal_approx(basis),
 		"%s is not equal to %s" % [str(expected_basis), str(basis)])
