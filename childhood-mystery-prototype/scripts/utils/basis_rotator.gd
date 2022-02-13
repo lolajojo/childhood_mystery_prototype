50097a43b47d972941ac2aacb0304d543b310059
Jojo Lolando <lolajojo@tutanota.com>
2022-02-13 20:04:29 +0300

    Fix y-axis head rotation

--- childhood-mystery-prototype/scripts/utils/basis_rotator.gd
+++ childhood-mystery-prototype/scripts/utils/basis_rotator.gd
@@ -21,11 +21,11 @@ func rotate_basis(
 		rotation_vector: Vector3,
 		min_rotation_vector: Vector3,
 		max_rotation_vector: Vector3) -> Basis:
-	var basis_rotation = basis.get_euler()
+	var basis_rotation = basis.orthonormalized().get_euler()
 	var angle_to_rotate = rotation_vector - basis_rotation
-	print("%s - %s = %s" % [str(rotation_vector), str(basis_rotation), str(angle_to_rotate)])
+#	print("%s - %s = %s" % [str(rotation_vector), str(basis_rotation), str(angle_to_rotate)])
 	
-	basis = _rotate_basis(basis, angle_to_rotate)
+	basis = _rotate_basis(basis.orthonormalized(), angle_to_rotate)
 	basis = _basis_corrector.correct_basis_with_limits(
 		basis, 
 		min_rotation_vector, 
@@ -34,13 +34,14 @@ func rotate_basis(
 	return basis
 
 func _rotate_basis(basis: Basis, rotation_angle_rad: Vector3):
-	print("before", basis)
+#	print("before", basis)
+	print(rotation_angle_rad)
 	basis = basis.rotated(basis.x, rotation_angle_rad.x)
 	print("x-rotated ", basis)
 	basis = basis.rotated(basis.y, rotation_angle_rad.y)
 	print("y-rotated ", basis)
 	basis = basis.rotated(basis.z, rotation_angle_rad.z)
 	print("z-rotated ", basis)
-	print("after", basis)
+#	print("after", basis)
 	
 	return basis
