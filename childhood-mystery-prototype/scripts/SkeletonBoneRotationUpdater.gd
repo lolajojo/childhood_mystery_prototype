ea7987acba11f41ae97e0ce78417085f33d12916
Jojo Lolando <lolajojo@tutanota.com>
2022-02-13 19:12:57 +0300

    Remove odd logic

--- childhood-mystery-prototype/scripts/SkeletonBoneRotationUpdater.gd
+++ childhood-mystery-prototype/scripts/SkeletonBoneRotationUpdater.gd
@@ -5,18 +5,19 @@ export(NodePath) var skeleton_path
 export(String) var bone_name
 
 var skeleton_node: Skeleton
-var _basis_corrector: LimitBasisCorrector
 
 export(Vector3) var min_rotation_vector = Vector3(-45, -90, -30)
 export(Vector3) var max_rotation_vector = Vector3(45, 90, 30)
 
+var _basis_rotator: BasisRotator
+
 func _ready():
 	skeleton_node = get_node(skeleton_path)
-	_basis_corrector = LimitBasisCorrector.new()
+	_basis_rotator = BasisRotator.new()
 
 
 func _exit_tree():
-	_basis_corrector.free()
+	_basis_rotator.free()
 
 
 func rotate_bone(rotation_vector: Vector3):
@@ -31,24 +32,14 @@ func rotate_bone(rotation_vector: Vector3):
 		return
 	
 	var bone_pose = skeleton_node.get_bone_global_pose(bone_id)
-	var bone_rotation = bone_pose.basis.get_euler()
-	var angle_to_rotate = rotation_vector - bone_rotation
-	print("%s - %s = %s" % [str(rotation_vector), str(bone_rotation), str(angle_to_rotate)])
+	bone_pose.basis = _basis_rotator.rotate_basis(
+		bone_pose.basis,
+		rotation_vector,
+		min_rotation_vector,
+		max_rotation_vector)
 	
-	bone_pose.basis = _rotate_basis(bone_pose.basis, angle_to_rotate)
-	bone_pose.basis = _basis_corrector.correct_basis_with_limits(bone_pose.basis, min_rotation_vector, max_rotation_vector)
-	
-	skeleton_node.set_bone_global_pose_override(bone_id, bone_pose, 1.0, true)
-
-
-func _rotate_basis(basis: Basis, rotation_angle_rad: Vector3):
-	print("before", basis)
-	basis = basis.rotated(basis.x, rotation_angle_rad.x)
-	print("x-rotated ", basis)
-	basis = basis.rotated(basis.y, rotation_angle_rad.y)
-	print("y-rotated ", basis)
-	basis = basis.rotated(basis.z, rotation_angle_rad.z)
-	print("z-rotated ", basis)
-	print("after", basis)
-	
-	return basis
+	skeleton_node.set_bone_global_pose_override(
+		bone_id, 
+		bone_pose, 
+		1.0, 
+		true)
