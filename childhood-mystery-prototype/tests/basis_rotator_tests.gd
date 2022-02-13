f2da4bc4b1808bfc60ba5c538e6b7304030cb474
Jojo Lolando <lolajojo@tutanota.com>
2022-02-13 18:41:07 +0300

    Add tests for the basis rotation logic

--- childhood-mystery-prototype/tests/basis_rotator_tests.gd
+++ childhood-mystery-prototype/tests/basis_rotator_tests.gd
@@ -4,6 +4,10 @@ extends WAT.Test
 var sut: BasisRotator
 
 
+const min_rotation = Vector3(-rad2deg(PI/2), -rad2deg(PI/2), -rad2deg(PI/2))
+const max_rotation = -min_rotation
+
+
 func pre():
 	sut = BasisRotator.new()
 
@@ -12,5 +16,37 @@ func post():
 	sut.free()
 
 
-func first_test():
-	pass
+func test_x_normale_rotation():
+	var expected_basis = Basis(Vector3.RIGHT, deg2rad(60))
+	var rotation_vector = Vector3(deg2rad(60), 0, 0)
+	
+	_run_rotation_with_assertion(expected_basis, rotation_vector)
+
+
+func test_y_normale_rotation():
+	var expected_basis = Basis(Vector3.UP, deg2rad(60))
+	var rotation_vector = Vector3(0, deg2rad(60), 0)
+	
+	_run_rotation_with_assertion(expected_basis, rotation_vector)
+
+
+func test_z_normale_rotation():
+	var expected_basis = Basis(Vector3.BACK, deg2rad(60))
+	var rotation_vector = Vector3(0, 0, deg2rad(60))
+	
+	_run_rotation_with_assertion(expected_basis, rotation_vector)
+
+# Tests Utility
+func _run_rotation_with_assertion(
+		expected_basis: Basis,
+		rotation_vector):
+	
+	var result_basis = sut.rotate_basis(
+		Basis(),
+		rotation_vector,
+		min_rotation,
+		max_rotation)
+	
+	asserts.is_true(
+		expected_basis.is_equal_approx(result_basis),
+		"%s is not equal to %s" % [str(expected_basis), str(result_basis)])
