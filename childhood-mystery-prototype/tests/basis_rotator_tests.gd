0a9e9f6c1265a037af46cdf595bde730fdddf6df
Jojo Lolando <lolajojo@tutanota.com>
2022-02-13 19:06:52 +0300

    Test rotation logic with multiple axis

--- childhood-mystery-prototype/tests/basis_rotator_tests.gd
+++ childhood-mystery-prototype/tests/basis_rotator_tests.gd
@@ -15,7 +15,7 @@ func pre():
 func post():
 	sut.free()
 
-
+# Test simple one axis rotation
 func test_x_normale_rotation():
 	var expected_basis = Basis(Vector3.RIGHT, deg2rad(60))
 	var rotation_vector = Vector3(deg2rad(60), 0, 0)
@@ -33,6 +33,27 @@ func test_y_normale_rotation():
 func test_z_normale_rotation():
 	var expected_basis = Basis(Vector3.BACK, deg2rad(60))
 	var rotation_vector = Vector3(0, 0, deg2rad(60))
+	
+	_run_rotation_with_assertion(expected_basis, rotation_vector)
+
+
+# Test multiple axis rotation
+func test_two_axis_rotation():
+	var expected_basis = Basis(
+		Vector3(0, 1, 0), 
+		Vector3(0, 0, -1), 
+		Vector3(-1, 0, 0))
+	var rotation_vector = Vector3(deg2rad(-90), deg2rad(-90), 0)
+	
+	_run_rotation_with_assertion(expected_basis, rotation_vector)
+
+
+func test_three_axis_rotation():
+	var expected_basis = Basis(
+		Vector3(0, 0, 1), 
+		Vector3(0, 1, 0), 
+		Vector3(-1, 0, 0))
+	var rotation_vector = Vector3(deg2rad(-90), deg2rad(-90), deg2rad(-90))
 	
 	_run_rotation_with_assertion(expected_basis, rotation_vector)
 
